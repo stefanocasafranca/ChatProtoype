87858c1e5969935261f4cf3a690934cf9f3fba40
stefanocasafranca <scasafrancal01@gmail.com>
2024-11-30 10:12:26 -0600

    Initial Commit

---
+++ ChatProtoype/ChatProtoypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatProtoypeApp.swift
+//  ChatProtoype
+//
+//  Created by Stefano Casafranca on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatProtoypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
